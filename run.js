18930be0e514093ef5cf14009f4bb34537c99567
Norbert Eder <wpfnerd+github@gmail.com>
2013-04-10 17:56:46 +0200

    Create config and export path if they do not exist

--- run.js
+++ run.js
@@ -15,5 +15,15 @@ settings.templatePath = path.join(settings.root, 'templates');
 settings.sprintTemplatePath = path.join(settings.root, 'templates' + path.sep + settings.template);
 settings.homeTemplatePath = path.join(settings.root, 'templates' + path.sep + settings.home_template);
 
+// if export path does not exist, create it
+if (!fs.existsSync(settings.exportPath)) {
+	fs.mkdirSync(settings.exportPath);
+}
+
+// if config path does not exist, create it
+if (!fs.existsSync(settings.configPath)) {
+	fs.mkdirSync(settings.configPath);
+}
+
 var server = require('./lib/server');
 require('http').createServer(server).listen(settings.port);
